71084bd19ed5e59a9d3922b186642a46077758d6
poriyabot <psharifaee@gmail.com>
2016-06-13 15:17:42 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {92441888,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -240,7 +240,7 @@ I M /-\ N (@Imandaneshi)
 Siyanew (@Siyanew)
 Rondoozle (@Potus)
 Seyedan (@Seyedan25)
-
+poriya_Amx
 Special thanks to:
 Juan Potato
 Siyanew
